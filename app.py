7791521d59dd350a0e9ee89e057385f50c83d6dd
TheRookiexe <mishrxa36@gmail.com>
2026-07-14 00:29:39 +0530

    initial flaks server working

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return "home"
+def home(): 
+    return render_template("index.html")
 
 
 if "__main__" == __name__:
