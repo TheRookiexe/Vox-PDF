72ab9ce07e40e43fd8c8313e3f419521eb6bd6c5
TheRookiexe <mishrxa36@gmail.com>
2026-07-16 20:47:34 +0530

    Add PDF upload pipeline and text extraction with pypdf

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from pathlib import Path
+from pypdf import PdfReader
 
 app = Flask(__name__)
 
@@ -16,14 +17,25 @@ def upload():
             'success': False,
             'message': 'No Pdf file was found!'
         }
-    
+    # featching and saving pdf
+
     uploaded_file = request.files["pdf"]
     upload_dir = "./uploads"
     file_name = secure_filename(uploaded_file.filename)
     path  = Path(upload_dir).joinpath(file_name)
     uploaded_file.save(path)
     print(path)
-    
+
+    # reading pdf
+    text = []
+    reader = PdfReader(path)
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text is not None:
+            text.append(page_text)
+    pdf_text = '\n\n'.join(text)
+    print(pdf_text)
+
     if uploaded_file.filename=='':
         return {
             'success': False,
