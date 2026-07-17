0cfaf9612f22a75e091b8f7ecb5f49cd5812c529
TheRookiexe <mishrxa36@gmail.com>
2026-07-17 16:30:51 +0530

    generate speech from uploaded PDF using Edge TTS

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from pathlib import Path
 from pypdf import PdfReader
+import edge_tts
+import asyncio
 
 app = Flask(__name__)
 
+async def generate_audio(text, output_file_path):
+    voice = "en-IN-NeerjaNeural"
+    comms = edge_tts.Communicate(text, voice)
+    await comms.save(output_file_path)
 
 @app.route('/')
 def home(): 
@@ -35,13 +41,17 @@ def upload():
             text.append(page_text)
     pdf_text = '\n\n'.join(text)
     print(pdf_text)
+    print(len(pdf_text))
 
     # writing outputs 
     output_dir = Path("./outputs")
     extension_mp3 = path.with_suffix(".mp3")
     output_file_name = extension_mp3.name
     output_path = output_dir.joinpath(output_file_name)
-    print(output_path)
+    
+    # converting text to audio
+    asyncio.run(generate_audio(text=pdf_text, output_file_path=output_path))
+
 
     if uploaded_file.filename=='':
         return {
