65eb59adc3acd76cb2a38c4c6f446400e5f17d11
TheRookiexe <mishrxa36@gmail.com>
2026-07-22 13:22:24 +0530

    Automate temporary files cleanup after download

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, after_this_request
 from werkzeug.utils import secure_filename
 from pathlib import Path
 from pypdf import PdfReader
@@ -23,8 +23,8 @@ def upload():
             'success': False,
             'message': 'No Pdf file was found!'
         }
+    
     # featching and saving pdf
-
     uploaded_file = request.files["pdf"]
     upload_dir = Path("./uploads")
     file_name = secure_filename(uploaded_file.filename)
@@ -40,7 +40,6 @@ def upload():
         if page_text is not None:
             text.append(page_text)
     pdf_text = '\n\n'.join(text)
-    print(pdf_text)
     print(len(pdf_text))
 
     # writing outputs 
@@ -58,7 +57,22 @@ def upload():
             'success': False,
             'message': 'The uploaded file is empty'
         }
+    
     print(uploaded_file.filename)
+
+    # Deleting the temp files
+    @after_this_request
+    def cleanup(response):
+        try:
+            path.unlink(missing_ok=True)
+        except Exception as e:
+            app.logger.exception(f"Error occured during deleting uploaded file: {e}")
+        try:
+            output_path.unlink(missing_ok=True)
+        except Exception as e:
+            app.logger.exception(f"Error occured during deleting output file: {e}")
+        return response
+
     return send_file(output_path, download_name=output_file_name, as_attachment=True)
     
 
